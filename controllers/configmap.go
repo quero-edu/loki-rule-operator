aa814dd6ae2fd999f5e3c965da79fa14ae5fa384
Marcos Toledo <toledompm@gmail.com>
2022-11-04 14:25:27 -0300

    feat: manage configmaps (#1)
    * feat: manage configmaps
    
    * created event handlers by type
    
    * update ConfigMap (#3)
    
    * update ConfigMap
    
    * fix types
    
    * use update instead of patch
    
    * feat: mount configmap (#4)
    
    * feat: attach volume to selected deployments
    
    * feat: unmount configmap on traveller deletion
    
    * style: var name typo
    
    Co-authored-by: dev-cruz <joaovictorcruz82@gmail.com>
    Co-authored-by: Henrique Manoel de Freitas Santos <henrique.manoel@hotmail.com.br>

---
+++ controllers/configmap.go
@@ -0,0 +1,147 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	mydomainv1alpha1 "github.com/quero-edu/loki-rule-operator/api/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// ensureConfigmap ensures Configmap exists in a namespace.
+func (r *TravellerReconciler) ensureConfigmapExists(
+	request reconcile.Request,
+	instance *mydomainv1alpha1.Traveller,
+	configmap *corev1.ConfigMap,
+) error {
+
+	found := &corev1.ConfigMap{}
+	err := r.Get(context.TODO(), types.NamespacedName{
+		Name:      configmap.Name,
+		Namespace: instance.Namespace,
+	}, found)
+
+	if err != nil && errors.IsNotFound(err) {
+		return r.Create(context.TODO(), configmap)
+	} else if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *TravellerReconciler) configMap(traveller *mydomainv1alpha1.Traveller) *corev1.ConfigMap {
+	labels := labels(traveller)
+
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      traveller.Spec.Name,
+			Namespace: traveller.Namespace,
+			Labels:    labels,
+		},
+		Data: traveller.Spec.Data,
+	}
+
+	controllerutil.SetControllerReference(traveller, configMap, r.Scheme)
+	return configMap
+}
+
+func (r *TravellerReconciler) ensureConfigMapIsAttached(
+	traveller *mydomainv1alpha1.Traveller,
+	configMap *corev1.ConfigMap,
+) error {
+	deployments, err := r.getTravellerTargetDeployments(traveller)
+
+	if err != nil {
+		return err
+	}
+
+	if len(deployments.Items) == 0 {
+		return fmt.Errorf("no deployments found for traveller %s", traveller.Name)
+	}
+
+	volumeName := fmt.Sprintf("%s-volume", configMap.Name)
+
+	volume := corev1.Volume{
+		Name: volumeName,
+		VolumeSource: corev1.VolumeSource{
+			ConfigMap: &corev1.ConfigMapVolumeSource{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: configMap.Name,
+				},
+			},
+		},
+	}
+
+	volumeMount := corev1.VolumeMount{
+		Name:      volumeName,
+		MountPath: traveller.Spec.MountPath,
+	}
+
+	for _, deployment := range deployments.Items {
+		if volumeExists(volume, deployment) && volumeIsMounted(volumeMount, deployment) {
+			continue
+		}
+
+		deployment.Spec.Template.Spec.Volumes = append(deployment.Spec.Template.Spec.Volumes, volume)
+
+		deployment.Spec.Template.Spec.Containers[0].VolumeMounts = append(
+			deployment.Spec.Template.Spec.Containers[0].VolumeMounts,
+			volumeMount,
+		)
+
+		err = r.Patch(context.TODO(), &deployment, client.Merge)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *TravellerReconciler) getTravellerTargetDeployments(
+	traveller *mydomainv1alpha1.Traveller,
+) (*appsv1.DeploymentList, error) {
+	selector, err := metav1.LabelSelectorAsSelector(&traveller.Spec.Selector)
+	if err != nil {
+		return nil, err
+	}
+
+	deployments := &appsv1.DeploymentList{}
+
+	err = r.List(context.TODO(), deployments, &client.ListOptions{
+		LabelSelector: selector,
+		Namespace:     "default",
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return deployments, nil
+}
+
+func volumeExists(volume corev1.Volume, deployment appsv1.Deployment) bool {
+	for _, v := range deployment.Spec.Template.Spec.Volumes {
+		if v.Name == volume.Name {
+			return true
+		}
+	}
+	return false
+}
+
+func volumeIsMounted(volumeMount corev1.VolumeMount, deployment appsv1.Deployment) bool {
+	for _, vm := range deployment.Spec.Template.Spec.Containers[0].VolumeMounts {
+		if vm.Name == volumeMount.Name {
+			return true
+		}
+	}
+	return false
+}
--- controllers/traveller_controller.go
+++ controllers/traveller_controller.go
@@ -18,11 +18,16 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	mydomainv1alpha1 "github.com/quero-edu/loki-rule-operator/api/v1alpha1"
 )
@@ -47,16 +52,104 @@ type TravellerReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *TravellerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx).WithValues("Traveller", req.NamespacedName)
 
-	// TODO(user): your logic here
+	log.Info(
+		fmt.Sprintf("Reconciling Traveller: %s", req.NamespacedName),
+	)
+
+	// Fetch the Traveller instance
+	instance := &mydomainv1alpha1.Traveller{}
+	err := r.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	configMap := r.configMap(instance)
+
+	err = r.ensureConfigmapExists(req, instance, configMap)
+	if err != nil {
+		log.Error(err, "Failed to ensure configmap exists")
+		return reconcile.Result{}, err
+	}
+
+	err = r.ensureConfigMapIsAttached(instance, configMap)
+	if err != nil {
+		log.Error(err, "ConfigMap not attached")
+		return reconcile.Result{}, err
+	}
+
+	err = r.Update(ctx, configMap)
+	if err != nil {
+		log.Error(err, "Failed to update configmap")
+		return reconcile.Result{}, err
+	}
+
+	log.Info("Traveller Reconciled")
 
 	return ctrl.Result{}, nil
+}
+
+func handleByEventType(r *TravellerReconciler) predicate.Predicate {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			return true
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return true
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			deletedInstance := e.Object.(*mydomainv1alpha1.Traveller)
+			r.deleteVolumesAndVolumeMounts(deletedInstance)
+			return false
+		},
+	}
+}
+
+func (r *TravellerReconciler) deleteVolumesAndVolumeMounts(v *mydomainv1alpha1.Traveller) error {
+	volumeName := generateVolumeName(v)
+
+	deployments, err := r.getTravellerTargetDeployments(v)
+	if err != nil {
+		return err
+	}
+
+	for _, deployment := range deployments.Items {
+		deployment.Spec.Template.Spec.Volumes = removeVolumeByName(deployment.Spec.Template.Spec.Volumes, volumeName)
+		deployment.Spec.Template.Spec.Containers[0] = removeVolumeMountByName(deployment.Spec.Template.Spec.Containers[0], volumeName)
+		err = r.Update(context.Background(), &deployment)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func removeVolumeByName(volumes []corev1.Volume, name string) []corev1.Volume {
+	for i, volume := range volumes {
+		if volume.Name == name {
+			return append(volumes[:i], volumes[i+1:]...)
+		}
+	}
+
+	return volumes
+}
+
+func removeVolumeMountByName(container corev1.Container, name string) corev1.Container {
+	for i, volumeMount := range container.VolumeMounts {
+		if volumeMount.Name == name {
+			container.VolumeMounts = append(container.VolumeMounts[:i], container.VolumeMounts[i+1:]...)
+		}
+	}
+
+	return container
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *TravellerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mydomainv1alpha1.Traveller{}).
+		WithEventFilter(handleByEventType(r)).
 		Complete(r)
 }

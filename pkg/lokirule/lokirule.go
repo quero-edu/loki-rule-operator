24a6fd748574542f5fae1613e8b6dab5cada363b
heitorrbarros@gmail.com <heitorrbarros@gmail.com>
2023-09-29 17:43:42 -0300

    feat: Mock handle validation

--- pkg/lokirule/lokirule.go
+++ pkg/lokirule/lokirule.go
@@ -2,13 +2,48 @@ package lokirule
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 
 	querocomv1alpha1 "github.com/quero-edu/loki-rule-operator/api/v1alpha1"
 	"gopkg.in/yaml.v2"
 )
 
-func GenerateRuleConfigMapFile(rule *querocomv1alpha1.LokiRule) (map[string]string, error) {
+type handleExpValidation func(expr string, lokiURL string) bool
+
+func ExprValid(expr string, lokiURL string) bool {
+
+	query := url.QueryEscape(expr)
+	url := lokiURL + "/loki/api/v1/query?query=" + query
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	if resp.StatusCode != 200 {
+		return false
+	}
+
+	return true
+}
+
+func GenerateRuleConfigMapFile(rule *querocomv1alpha1.LokiRule, expValidation handleExpValidation) (map[string]string, error) {
 	fileName := fmt.Sprintf("%s-%s.yaml", rule.Namespace, rule.Name)
+
+	if expValidation == nil {
+		expValidation = ExprValid
+	}
+
+	for _, group := range rule.Spec.Groups {
+		for _, ruleGroup := range group.Rules {
+			if !expValidation(ruleGroup.Expr, "https://fake.loki.url") {
+				return nil, fmt.Errorf("have an error on your LogQL %s", ruleGroup.Expr)
+			}
+		}
+	}
 
 	marshaledGroupData, err := yaml.Marshal(rule.Spec)
 	if err != nil {
--- pkg/lokirule/lokirule_test.go
+++ pkg/lokirule/lokirule_test.go
@@ -1,6 +1,10 @@
 package lokirule
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
@@ -14,6 +18,43 @@ import (
 func TestLokiRule(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "LokiRule Suite")
+}
+
+func TestExprValid(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	}))
+
+	defer server.Close()
+
+	isValid := ExprValid("test", "server.URL")
+
+	if isValid == true {
+		t.Errorf("Expected false, got %t", isValid)
+	}
+
+}
+
+func ExampleResponseRecorder() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+
+	req := httptest.NewRequest("GET", "https://loki-front.mock.test", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+	// Output:
+	// 200
+	// text/html; charset=utf-8
+	// <html><body>Hello World!</body></html>
 }
 
 var _ = Describe("TestGenerateRuleConfigMapFile", func() {
@@ -53,7 +94,11 @@ var _ = Describe("TestGenerateRuleConfigMapFile", func() {
 			},
 		}
 
-		ruleFile, err := GenerateRuleConfigMapFile(rule)
+		handleExpValidationMock := func(expr string, lokiURL string) bool {
+			return true
+		}
+
+		ruleFile, err := GenerateRuleConfigMapFile(rule, handleExpValidationMock)
 		Expect(err).To(BeNil())
 
 		parsedKeys := []string{}
@@ -80,5 +125,33 @@ var _ = Describe("TestGenerateRuleConfigMapFile", func() {
 		)
 
 		Expect(reflect.DeepEqual(parsedRuleFileContent, expectedParsedYamlContent)).To(BeTrue())
+	})
+
+	It("should generate an erro of Expr", func() {
+		rule := &querocomv1alpha1.LokiRule{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-rule",
+				Namespace: "test-namespace",
+			},
+			Spec: querocomv1alpha1.LokiRuleSpec{
+				Groups: []querocomv1alpha1.RuleGroup{
+					{
+						Name: "test-namespace-test-rule",
+						Rules: []querocomv1alpha1.Rule{
+							{
+								Record: "test_record",
+								Expr:   "INVALID_EXPR",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		handleExpValidationMock := func(expr string, lokiURL string) bool {
+			return false
+		}
+		_, err := GenerateRuleConfigMapFile(rule, handleExpValidationMock)
+		Expect(err).To(Equal(fmt.Errorf("have an error on your LogQL %s", rule.Spec.Groups[0].Rules[0].Expr)))
 	})
 })
